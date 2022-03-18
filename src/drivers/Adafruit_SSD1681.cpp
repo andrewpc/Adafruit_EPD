3c3d3bfdac1508a2f737d812c373c48ba261713f
Andrew Carey <acarey.au@gmail.com>
2022-03-18 13:59:26 +1100

    use yield

--- src/drivers/Adafruit_SSD1681.cpp
+++ src/drivers/Adafruit_SSD1681.cpp
@@ -107,7 +107,7 @@ void Adafruit_SSD1681::busy_wait(void) {
   if (_busy_pin >= 0) {
     while (digitalRead(_busy_pin)) { // wait for busy low
       //Serial.println("in busy_wait loop");
-      delay(1);
+      yield();
     }
   } else {
     delay(BUSY_WAIT);
